a899b16e03f7b2740423b60b7c6758e8fa0e251f
aburk3 <aburk3@gmail.com>
2019-08-13 06:52:08 -0500

    Added basic spring-demo project files

---
+++ src/com/luv2code/springdemo/BadmittenCoach.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo;
+
+public class BadmittenCoach implements Coach {
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Make sure to practice badding some mitten today!!!";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
