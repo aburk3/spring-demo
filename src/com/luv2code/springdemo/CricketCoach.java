365fb5d231b90a161c9c0e5119e78b7fee9940f3
aburk3 <aburk3@gmail.com>
2019-08-13 18:32:37 -0500

    Created setter method for fortuneService

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -9,6 +9,10 @@ public class CricketCoach implements Coach {
 		System.out.println("CricketCoach: inside no-arg constructor");
 	}
 	
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Practice fast bowling for 15 minutes";
