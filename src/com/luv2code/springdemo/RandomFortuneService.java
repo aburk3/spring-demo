502846fd6ad6144a7b8d845858912d18cdd4f194
aburk3 <aburk3@gmail.com>
2019-08-14 06:36:09 -0500

    Created Array of strings

--- src/com/luv2code/springdemo/RandomFortuneService.java
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -1,7 +1,16 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 public class RandomFortuneService implements FortuneService {
 
+	// create an array of strings
+	private String[] data = {
+			"Beware of the wolf in sheep's clothing",
+			"Diligence is the mother of good luck",
+			"The journey is the reward"
+	};
+	
 	@Override
 	public String getFortune() {
 		return null;
