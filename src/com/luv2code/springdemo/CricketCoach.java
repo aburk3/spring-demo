82d7983d99c58195fff68161be2c6243aabc1a85
aburk3 <aburk3@gmail.com>
2019-08-14 06:01:21 -0500

    Added new getters and setters for emailAddress and Team

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -4,6 +4,26 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	// add new fields for emailAddress and team
+	private String emailAddress;
+	private String team;
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
 	// create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
